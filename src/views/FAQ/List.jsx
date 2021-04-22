ec2d738cfc391441aa0d615e805c6ca14beb0640
Arslan Shahab <arslanshahab@gmail.com>
2021-04-22 12:53:53 +0500

    fixed add faq api

--- src/views/FAQ/List.jsx
+++ src/views/FAQ/List.jsx
@@ -1,13 +1,19 @@
-import { Button, Typography } from '@material-ui/core';
-import React, { useContext, useEffect, useState } from 'react';
-import { AccordionContext, Card } from 'react-bootstrap';
-import Accordion from 'react-bootstrap/Accordion';
-import { useParams } from 'react-router-dom';
-import API from 'utils/http';
-import AddFAQDialog from './AddFAQDialog';
+import { Button, Typography } from "@material-ui/core";
+import React, { useContext, useEffect, useState } from "react";
+import { AccordionContext, Card } from "react-bootstrap";
+import Accordion from "react-bootstrap/Accordion";
+import { useParams } from "react-router-dom";
+import API from "utils/http";
+import AddFAQDialog from "./AddFAQDialog";
 
-
-function ContextAwareToggle({ children, eventKey, callback, sectionIndex, contentIndex, handleDelete }) {
+function ContextAwareToggle({
+  children,
+  eventKey,
+  callback,
+  sectionIndex,
+  contentIndex,
+  handleDelete,
+}) {
   const currentEventKey = useContext(AccordionContext);
 
   // const decoratedOnClick = useAccordionToggle(
@@ -19,11 +25,18 @@ function ContextAwareToggle({ children, eventKey, callback, sectionIndex, conten
 
   return (
     <div className="d-flex align-items-center justify-content-center">
-      <i style={{ width: '5%' }} className={`pr-4 fa ${isCurrentEventKey ? 'fa-minus' : 'fa-plus'}`} />
-      <h6 style={{ padding: '0.5rem 0', marginBottom: 0, width: '90%' }}>
+      <i
+        style={{ width: "5%" }}
+        className={`pr-4 fa ${isCurrentEventKey ? "fa-minus" : "fa-plus"}`}
+      />
+      <h6 style={{ padding: "0.5rem 0", marginBottom: 0, width: "90%" }}>
         {children}
       </h6>
-      <i style={{ width: '5%', color: '#ff0000' }} className={`fa fa-trash float-right d-block`} onClick={() => handleDelete(sectionIndex, contentIndex)} />
+      <i
+        style={{ width: "5%", color: "#ff0000" }}
+        className={`fa fa-trash float-right d-block`}
+        onClick={() => handleDelete(sectionIndex, contentIndex)}
+      />
     </div>
   );
 }
@@ -36,39 +49,44 @@ const FAQList = (props) => {
   const [showFAQ, setShowFAQ] = useState(false);
   const [faq, setFAQ] = useState({
     id: 0,
-    section_name: '',
-    section_content: [{
-      question: '', answer: ''
-    }],
+    section_name: "",
+    section_content: [
+      {
+        question: "",
+        answer: "",
+      },
+    ],
     page_id: pageId,
-    section_avatar: '',
+    section_avatar: "",
     section_col_arr: 0,
     section_prior: 1,
-    section_avtar_alt: '',
-    section_slug: 'faq'
-  })
+    section_avtar_alt: "",
+    section_slug: "faq",
+  });
 
   useEffect(() => {
-    API.get('/faqs').then(response => {
-      if (response.status === 200) {
-        setFaqList(response.data)
-      }
-    }).catch(err => {
-      alert("Something went wrong")
-    })
-  }, [])
+    API.get("/faqs")
+      .then((response) => {
+        if (response.status === 200) {
+          setFaqList(response.data);
+        }
+      })
+      .catch((err) => {
+        alert("Something went wrong");
+      });
+  }, []);
 
   //faq section methods
   const handleQuestionChange = (e, index) => {
     let section_content = [...faq.section_content];
     section_content[0].question = e.target.value;
-    setFAQ({ ...faq, section_content })
-  }
+    setFAQ({ ...faq, section_content });
+  };
   const handleAnswerChange = (data, index) => {
     let section_content = [...faq.section_content];
     section_content[0].answer = data;
-    setFAQ({ ...faq, section_content })
-  }
+    setFAQ({ ...faq, section_content });
+  };
   //end faq section methods
 
   const handleDelete = (sectionIndex, contentIndex) => {
@@ -76,23 +94,27 @@ const FAQList = (props) => {
     //parsing to JSON as the data is stringified
     let updatedSectionContent = JSON.parse(updatedFAQ.section_content);
     //deleting index item
-    updatedSectionContent = updatedSectionContent.filter((x, i) => i !== contentIndex);
+    updatedSectionContent = updatedSectionContent.filter(
+      (x, i) => i !== contentIndex
+    );
     //replacing section content with modified array
     updatedFAQ.section_content = updatedSectionContent;
 
-    API.put(`/add_section/${updatedFAQ.page_id}`, updatedFAQ).then(response => {
-      if (response.status === 200) {
-        alert("FAQ added successfully !");
-      }
-    }).then(()=>{
-      API.get('/faqs').then(response => {
+    API.put(`/add_section/${updatedFAQ.page_id}`, updatedFAQ)
+      .then((response) => {
         if (response.status === 200) {
-          setFaqList(response.data)
+          alert("FAQ added successfully !");
         }
       })
-    })
-    .catch(err => console.log(err))
-  }
+      .then(() => {
+        API.get("/faqs").then((response) => {
+          if (response.status === 200) {
+            setFaqList(response.data);
+          }
+        });
+      })
+      .catch((err) => console.log(err));
+  };
 
   const handleSubmit = () => {
     let updatedFAQ = faqList[currentFAQ.index];
@@ -105,62 +127,75 @@ const FAQList = (props) => {
     updatedFAQ.section_content = updatedSectionContent;
 
     //extracting current pageId and calling update API
-    API.put(`/add_section/${updatedFAQ.page_id}`, updatedFAQ).then(response => {
-      if (response.status === 200) {
-        alert("FAQ added successfully !");
-      }
-    }).then(()=>{
-      API.get('/faqs').then(response => {
+    API.post(`/add_section`, updatedFAQ)
+      .then((response) => {
         if (response.status === 200) {
-          setFaqList(response.data)
+          alert("FAQ added successfully !");
+          setShowFAQ(false);
         }
       })
-    })
-    .catch(err => console.log(err))
-  }
+      .then(() => {
+        API.get("/faqs").then((response) => {
+          if (response.status === 200) {
+            setFaqList(response.data);
+          }
+        });
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="faq-section-block my-3 my-sm-4">
       <div className="container">
-        <h3 className="text-center main-title mb-3">Frequently Asked Questions (F.A.Q's)</h3>
+        <h3 className="text-center main-title mb-3">
+          Frequently Asked Questions (F.A.Q's)
+        </h3>
 
-        {
-          faqList?.map((faq, i) => (
-            <div key={faq.id}>
-              {
-                <div className="d-flex justify-content-between align-items-center">
-                  <h5 className="my-3">
-                    {faq.post_name}
-                  </h5>
-                  <Button variant="contained" color="primary" size="small" onClick={() => {
+        {faqList?.map((faq, i) => (
+          <div key={faq.id}>
+            {
+              <div className="d-flex justify-content-between align-items-center">
+                <h5 className="my-3">{faq.post_name}</h5>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="small"
+                  onClick={() => {
                     setCurrentFAQ({ ...faq, index: i });
                     setShowFAQ(true);
-                  }}>
-                    Add New F.A.Q Item
-                  </Button>
-                </div>
-              }
-              {
-                JSON.parse(faq.section_content).map((x, ind) => (
-                  <Accordion key={x.id}>
-                    <Card>
-                      <Accordion.Toggle as={Card.Header} eventKey={`${i}`} style={{ cursor: 'pointer' }} >
-                        <ContextAwareToggle eventKey={`${i}`} sectionIndex={i} contentIndex={ind} handleDelete={handleDelete} >
-                          {x.question}
-                        </ContextAwareToggle>
-                      </Accordion.Toggle>
-                      <Accordion.Collapse eventKey={`${i}`}>
-                        <Card.Body>
-                          <div dangerouslySetInnerHTML={{ __html: x.answer }}></div>
-                        </Card.Body>
-                      </Accordion.Collapse>
-                    </Card>
-                  </Accordion>
-                ))
-              }
-            </div>
-          ))
-        }
+                  }}
+                >
+                  Add New F.A.Q Item
+                </Button>
+              </div>
+            }
+            {JSON.parse(faq.section_content).map((x, ind) => (
+              <Accordion key={x.id}>
+                <Card>
+                  <Accordion.Toggle
+                    as={Card.Header}
+                    eventKey={`${i}`}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <ContextAwareToggle
+                      eventKey={`${i}`}
+                      sectionIndex={i}
+                      contentIndex={ind}
+                      handleDelete={handleDelete}
+                    >
+                      {x.question}
+                    </ContextAwareToggle>
+                  </Accordion.Toggle>
+                  <Accordion.Collapse eventKey={`${i}`}>
+                    <Card.Body>
+                      <div dangerouslySetInnerHTML={{ __html: x.answer }}></div>
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
+            ))}
+          </div>
+        ))}
       </div>
       {
         <AddFAQDialog
@@ -173,8 +208,8 @@ const FAQList = (props) => {
           open={showFAQ}
         />
       }
-    </div >
+    </div>
   );
-}
+};
 
 export default FAQList;
